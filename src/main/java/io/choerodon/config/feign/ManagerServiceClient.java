be1a28715c3bd622f4fe437d4893ed82f7225929
flyleft <jcalaz@163.com>
2018-06-12 17:41:25 +0800

    [IMP] 修复了配置中kafka错误；修改了向manager-server获取配置的feign请求url

--- src/main/java/io/choerodon/config/feign/ManagerServiceClient.java
+++ src/main/java/io/choerodon/config/feign/ManagerServiceClient.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 调用manager管理服务的feign
@@ -19,7 +18,7 @@ public interface ManagerServiceClient {
      * @param serviceName 应用名
      * @return 配置集
      */
-    @RequestMapping(value = "/v1/configs/{service_name}/default", method = RequestMethod.GET)
+    @RequestMapping(value = "/v1/configs/{service_name}/configs/default", method = RequestMethod.GET)
     Config getConfig(@PathVariable("service_name") String serviceName);
 
     /**
@@ -28,7 +27,7 @@ public interface ManagerServiceClient {
      * @param configVersion 配置版本
      * @return 配置集
      */
-    @RequestMapping(value = "/v1/configs/{serviceName}/by_version", method = RequestMethod.GET)
-    Config getConfig(@PathVariable("serviceName") String serviceName,  @RequestParam("config_version") String configVersion);
+    @RequestMapping(value = "/v1/configs/{service_name}/configs/{config_version}", method = RequestMethod.GET)
+    Config getConfig(@PathVariable("service_name") String serviceName,  @PathVariable("config_version") String configVersion);
 
 }
